31fd3afc6b5c87df9172659014e29b3840b51dce
ikura <142256317+ikura-ee@users.noreply.github.com>
2023-09-03 03:33:23 +0900

    Update 変数.js

--- 変数.js
+++ 変数.js
@@ -95,3 +95,14 @@ var りほたん = { 性別: "女性", 年齢:19, 種族:"天使"};
 
 console.log(HP)
 console.log(AT)
+
+
+var りほたん = { 性別 : "女性", 年齢 : 19, 種族 : "天使", HP:9999999,AT:9999999};
+var enemy = ["ケンシロウ1","ケンシロウ2","ケンシロウ3","ケンシロウ4","ケンシロウ5"];
+enemy.種族 = "陰獣"
+enemy.HP = 1000
+enemy.AT = 600
+
+for ( i = 0, enemy.length; i++);
+console.log(りほたん）
+
